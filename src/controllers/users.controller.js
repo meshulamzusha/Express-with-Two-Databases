b8269bc1787b7ffdcc55858dad8782f9d1e9e286
Your Name <you@example.com>
2025-12-31 17:42:59 +0200

    Deleting validation logic in registerUser controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,24 +4,6 @@ async function registerUser(req, res) {
     try {
         const { username, password } = req.body;
 
-        if (!username || !password) {
-            return res.status(400).json({
-                message: "username or password is missing in request body."
-            })
-        }
-
-        if (typeof username != 'string') {
-            return res.status(400).json({
-                message: "Type of username must be string"
-            })
-        }
-
-        if (typeof password != 'string') {
-            return res.status(400).json({
-                message: "Type of password must be string"
-            })
-        }
-
         const error = await service.registerUserService(username, password);
 
         if (error) {
